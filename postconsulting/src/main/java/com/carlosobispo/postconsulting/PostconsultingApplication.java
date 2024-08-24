d2c7424341b6a895b6c2cba976d414e221129197
ZBishop <obisporicapa@gmail.com>
2024-08-24 18:47:29 -0500

    Project Init
    SpringBoot init

---
+++ postconsulting/src/main/java/com/carlosobispo/postconsulting/PostconsultingApplication.java
@@ -0,0 +1,13 @@
+package com.carlosobispo.postconsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostconsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostconsultingApplication.class, args);
+	}
+
+}
---
+++ postconsulting/src/test/java/com/carlosobispo/postconsulting/PostconsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosobispo.postconsulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostconsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
